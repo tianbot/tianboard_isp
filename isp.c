34d27f444c06d0d025c173bd45858f53e379f757
Xu jiangbo <xjbwd@163.com>
2020-12-09 17:52:17 +0800

    temp version, need to be fixed

--- isp.c
+++ isp.c
@@ -1,6 +1,9 @@
 #include <isp.h>
 #include <wiringSerial.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 static int fd;
 static stm32info_t stm32info;
 
@@ -124,9 +127,213 @@ void isp_close()
 void isp_reboot()
 {
     printf("reboot ");
-    serialPrintf(fd, "update");
+
+    int i;
+    char buf[] = "update";
+    unsigned int len;
+    unsigned short cmd = 0x4000;
+    unsigned char bcc = 0;
+
+    serialPutchar(fd, 0x55);
+    serialPutchar(fd, 0xAA);
+
+    len = 2 + strlen(buf);
+
+    serialPutchar(fd, len & 0xFF);
+    serialPutchar(fd, (len >> 8) & 0xFF);
+
+    serialPutchar(fd, cmd & 0xFF);
+    serialPutchar(fd, (cmd >> 8) & 0xFF);
+    bcc ^= cmd & 0xFF;
+    bcc ^= (cmd >> 8) & 0xFF;
+    for (i = 0; i < strlen(buf); i++)
+    {
+        serialPutchar(fd, buf[i]);
+        bcc ^= buf[i];
+    }
+
+    serialPutchar(fd, bcc);
     usleep(1000000);
     printf(GREEN "[done]\n" NONE);
+}
+
+char *get_response(unsigned char *buff, unsigned int len)
+{
+#if 0
+    static char response[128];
+    static uint8_t state = 0;
+    uint8_t *p = data;
+    static vector<uint8_t> recv_msg;
+    static uint32_t len;
+    uint32_t j;
+
+    while (data_len != 0)
+    {
+        switch (state)
+        {
+        case 0:
+            if (*p == (PROTOCOL_HEAD & 0xFF))
+            {
+                recv_msg.clear();
+                recv_msg.push_back(PROTOCOL_HEAD & 0xFF);
+                state = 1;
+            }
+            p++;
+            data_len--;
+            break;
+
+        case 1:
+            if (*p == ((PROTOCOL_HEAD >> 8) & 0xFF))
+            {
+                recv_msg.push_back(((PROTOCOL_HEAD >> 8) & 0xFF));
+                p++;
+                data_len--;
+                state = 2;
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+
+        case 2: // len
+            recv_msg.push_back(*p);
+            len = *p;
+            p++;
+            data_len--;
+            state = 3;
+            break;
+
+        case 3: // len
+            recv_msg.push_back(*p);
+            len += (*p) * 256;
+            if (len > 1024 * 10)
+            {
+                state = 0;
+                break;
+            }
+            p++;
+            data_len--;
+            state = 4;
+            break;
+
+        case 4: // pack_type
+            recv_msg.push_back(*p);
+            p++;
+            data_len--;
+            len--;
+            state = 5;
+            break;
+
+        case 5: // pack_type
+            recv_msg.push_back(*p);
+            p++;
+            data_len--;
+            len--;
+            state = 6;
+            break;
+
+        case 6: //
+            if (len--)
+            {
+                recv_msg.push_back(*p);
+                p++;
+                data_len--;
+            }
+            else
+            {
+                state = 7;
+            }
+            break;
+
+        case 7:
+        {
+            int i;
+            uint8_t bcc = 0;
+            recv_msg.push_back(*p);
+            p++;
+            data_len--;
+            state = 0;
+
+            for (i = 4; i < recv_msg.size(); i++)
+            {
+                bcc ^= recv_msg[i];
+            }
+
+            if (bcc == 0)
+            {
+                tianbotDataProc(&recv_msg[0], recv_msg.size()); // process recv msg
+                communication_timer_.stop();                    // restart timer for communication timeout
+                communication_timer_.start();
+            }
+            else
+            {
+                ROS_INFO("BCC error");
+            }
+            state = 0;
+        }
+        break;
+
+        default:
+            state = 0;
+            break;
+        }
+    }
+#endif
+}
+
+void isp_get_version(void)
+{
+    int i = 0;
+    int recv_cnt = 100;
+    static char buf[128] = "version";
+    unsigned int len;
+    unsigned short cmd = 0x4000;
+    unsigned char bcc = 0;
+
+    serialPutchar(fd, 0x55);
+    serialPutchar(fd, 0xAA);
+
+    len = 2 + strlen(buf);
+
+    serialPutchar(fd, len & 0xFF);
+    serialPutchar(fd, (len >> 8) & 0xFF);
+
+    serialPutchar(fd, cmd & 0xFF);
+    serialPutchar(fd, (cmd >> 8) & 0xFF);
+    bcc ^= cmd & 0xFF;
+    bcc ^= (cmd >> 8) & 0xFF;
+    for (i = 0; i < strlen(buf); i++)
+    {
+        serialPutchar(fd, buf[i]);
+        bcc ^= buf[i];
+    }
+
+    serialPutchar(fd, bcc);
+
+    do
+    {
+        usleep(1000);
+        int len = serialDataAvail(fd);
+        if (len != 0)
+        {
+            i = 0;
+
+            while ((len--) && (i < sizeof(buf)))
+            {
+                buf[i] = serialGetchar(fd);
+                i++;
+            }
+            get_response(buf, i);
+        }
+    } while ((i < sizeof(buf)) && recv_cnt);
+    if (recv_cnt == 0)
+    {
+        printf(RED "Get version failed\r\n" NONE);
+        exit(-1);
+    }
+    usleep(100000);
+    printf(GREEN "[%s]\n" NONE, buf);
 }
 
 int isp_sync()
@@ -184,7 +391,7 @@ int isp_get_command(void)
     {
         printf("0x%02x ", stm32info.cmd[i]);
     }
-    printf(LEFT_1_C "]"NONE "\n");
+    printf(LEFT_1_C "]" NONE "\n");
     return 0;
 }
 
@@ -230,10 +437,10 @@ int isp_write_bin(FILE *fp)
     int filesize;
 
     fseek(fp, 0L, SEEK_END);
-	filesize = ftell(fp);
+    filesize = ftell(fp);
     rewind(fp);
 
-    printf("download %3d%%\n", offset*100/filesize);
+    printf("download %3d%%\n", offset * 100 / filesize);
 
     while (1)
     {
@@ -246,14 +453,14 @@ int isp_write_bin(FILE *fp)
         if (i == 256)
         {
             isp_write_block(buf, 0x08000000 + offset, 256);
-            printf(UP_LINE "download %3d%%\n", offset*100/filesize);
+            printf(UP_LINE "download %3d%%\n", offset * 100 / filesize);
             i = 0;
             offset += 256;
         }
     }
     isp_write_block(buf, 0x08000000 + offset, i);
     offset += i;
-    printf(UP_LINE "download %3d%% " GREEN "[done]\n" NONE, offset*100/filesize);
+    printf(UP_LINE "download %3d%% " GREEN "[done]\n" NONE, offset * 100 / filesize);
     return 0;
 }
 
@@ -265,10 +472,10 @@ int isp_verify(FILE *fp)
     int filesize;
 
     fseek(fp, 0L, SEEK_END);
-	filesize = ftell(fp);
+    filesize = ftell(fp);
     rewind(fp);
 
-    printf("verify %3d%%\n", offset*100/filesize);
+    printf("verify %3d%%\n", offset * 100 / filesize);
 
     while (1)
     {
@@ -289,7 +496,7 @@ int isp_verify(FILE *fp)
                     return -1;
                 }
             }
-            printf(UP_LINE "verify %3d%%\n", offset*100/filesize);
+            printf(UP_LINE "verify %3d%%\n", offset * 100 / filesize);
             i = 0;
             offset += 256;
         }
@@ -305,55 +512,8 @@ int isp_verify(FILE *fp)
         }
     }
     offset += i;
-    printf(UP_LINE "verify %3d%% " GREEN "[done]\n" NONE, offset*100/filesize);
+    printf(UP_LINE "verify %3d%% " GREEN "[done]\n" NONE, offset * 100 / filesize);
     return 0;
-}
-
-void isp_get_version(void)
-{
-    int i = 0;
-    int recv_cnt = 5;
-
-    char buf[128];
-
-    printf("get firmware version ");
-
-    do
-    {
-        serialPrintf(fd, "imu stop");
-        usleep(10000);
-        serialFlush(fd);
-        usleep(10000);
-    } while (serialDataAvail(fd) != 0);
-
-    serialPrintf(fd, "ver");
-    usleep(100000);
-    do
-    {
-        int len = serialDataAvail(fd);
-        if (len != 0)
-        {
-            while ((len--) && (i < sizeof(buf)))
-            {
-                buf[i] = serialGetchar(fd);
-                if (buf[i] == '\0' || buf[i] == '\x0a')
-                {
-                    buf[i] = '\0';
-                    break;
-                }
-                i++;
-            }
-        }
-        else
-        {
-            recv_cnt--;
-        }
-        usleep(10000);
-    } while ((i < sizeof(buf)) && recv_cnt);
-
-    serialPrintf(fd, "imu start");
-    usleep(100000);
-    printf(GREEN "[%s]\n" NONE, buf);
 }
 
 void isp_go_app(void)
